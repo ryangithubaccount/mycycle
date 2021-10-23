9b7ba94ad12a633a55a3e292c093eba80bf6c81c
Laura <lauraspberrypie@gmail.com>
2021-10-23 13:53:21 -0700

    added auth functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import { useHistory } from "react-router-dom";
 //import firebase from "../node_modules/firebase/app";
 // import firebase from "firebase";
 
-
-// firebase.initializeApp(); 
-
-
-
+// firebase.initializeApp(); d
+class LoginForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { username: '', password: '' };
+  }
+  
+  handleChange = ({target}) => {
+    this.setState({ [target.name]: target.value });
+  };
+  
+  render() {
+    return (
+      <React.Fragment>
+        <form>
+          <label htmlFor="username">username</label>
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+        </form>
+        <form>
+          <label htmlFor="password">password</label>
+          <input
+            type="text"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+        </form>
+        <h3>Your username is: {this.state.username}</h3>
+        <button>Sign In</button>
+      </React.Fragment>
+    );
+  }
+}
 
 function App() {
   return (
@@ -17,17 +52,13 @@ function App() {
         <p>
           Welcome to MyCycle!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Log In
-        </a>
+        <div className="login-form">
+          <LoginForm />
+        </div>
       </header>
     </div>
   );
 }
+
 
 export default App;
