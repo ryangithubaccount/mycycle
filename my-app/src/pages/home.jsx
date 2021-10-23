fa68cac0a0fb9649186d3a4c5b481d31e72b3f97
rchan <rchan@caltech.edu>
2021-10-23 15:10:59 -0700

    Added multiple page functionality

--- my-app/src/pages/home.jsx
+++ my-app/src/pages/home.jsx
@@ -2,14 +2,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 //Functional Component 
-const MainPage = () => {
+const HomePage = () => {
   return (
     <div>
-      <h3>Welcome to the React Router Tutorial</h3>
+      <h3>Welcome to the Home Page</h3>
       <small>Main Page</small>
       <Link to="/users">Show List of Users</Link>
     </div>
   );
 };
 
-export default MainPage;
+export default HomePage;
---
+++ my-app/src/pages/login.jsx
@@ -0,0 +1,60 @@
+/*login.jsx*/
+import React from "react";
+import { Link } from "react-router-dom";
+
+//Functional Component 
+
+class LoginForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { username: '', password: '' };
+    }
+
+    handleChange = ({target}) => {
+        this.setState({ [target.name]: target.value });
+    };
+    render() {
+    return (
+        <React.Fragment>
+            <form>
+            <label htmlFor="username">username</label>
+            <input
+                type="text"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleChange}
+            />
+            </form>
+            <form>
+            <label htmlFor="password">password</label>
+            <input
+                type="text"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+            />
+            </form>
+            <h3>Your username is: {this.state.username}</h3>
+            <button>Sign In</button>
+            <Link to="/home">SIGN IN</Link>
+        </React.Fragment>
+        );
+    }
+}
+
+const LoginPage = () => {
+    return(
+        <div className="LogIn">
+        <header className="LogIn-header">
+            <p>
+            Welcome to MyCycle!
+            </p>
+            <div className="login-form">
+                <LoginForm/>
+            </div>
+        </header>
+        </div>
+    );
+};
+
+export default LoginPage;
